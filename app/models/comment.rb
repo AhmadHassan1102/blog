9add756cbbd677fdc990262640431e7816139e7e
AhmadHassan1102 <ahmad.alihussain289@gmail.com>
2022-07-20 21:34:53 +0500

    FirstRails

---
+++ app/models/comment.rb
@@ -0,0 +1,9 @@
+
+class Comment < ApplicationRecord
+  include Visible
+  validates :commenter, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+
+  belongs_to :article
+end
+
